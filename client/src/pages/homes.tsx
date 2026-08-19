7aecb71bf3bb27190e970d9892aa0416046ffa80
infotradescout <info.tradescout@gmail.com>
2026-08-19 14:23:59 -0500

    Route Property Blessings HomeID to launch control

--- client/src/pages/homes.tsx
+++ client/src/pages/homes.tsx
@@ -1,1 +1,18 @@
-export { default } from "./homeid/HomeIdWorkspace";
+import HomeIdWorkspace from "./homeid/HomeIdWorkspace";
+import PropertyBlessingsLaunchWorkspace from "./homeid/PropertyBlessingsLaunchWorkspace";
+
+const PROPERTY_BLESSINGS_HOME_ID = "073b355c-1aa3-4658-a776-ebedaa6aaefc";
+
+export default function Homes() {
+  if (typeof window === "undefined") return <HomeIdWorkspace />;
+
+  const params = new URLSearchParams(window.location.search);
+  const homeId = params.get("homeId");
+  const mode = params.get("mode");
+
+  return homeId === PROPERTY_BLESSINGS_HOME_ID && mode !== "passport" ? (
+    <PropertyBlessingsLaunchWorkspace />
+  ) : (
+    <HomeIdWorkspace />
+  );
+}
